27fc6cff47621cc473d67eefd1430c88ca7d1ad9
Rhonda Kremer <rhondakremer@gmail.com>
2020-07-11 14:19:58 -0400

    multiply, divide, some basic error handling

--- flashcard/website/views.py
+++ flashcard/website/views.py
@@ -13,6 +13,18 @@ def add(request):
         old_num_1 = request.POST['old_num_1']
         old_num_2 = request.POST['old_num_2']
 
+        # error handling if no answer submitted
+        if not answer: 
+            my_answer = "Please enter a valid number."
+            color = 'warning'
+            return render(request, 'add.html', {
+            'answer': answer,
+            'my_answer': my_answer,
+            'num_1': num_1,
+            'num_2': num_2,
+            'color': color
+        })
+
         correct_answer = int(old_num_1) + int(old_num_2)
         if int(answer) == correct_answer:
             my_answer = "Correct! " + old_num_1 + " + " + old_num_2 + " = " + str(correct_answer)
@@ -43,6 +55,18 @@ def subtract(request):
         old_num_1 = request.POST['old_num_1']
         old_num_2 = request.POST['old_num_2']
 
+        # error handling if no answer submitted
+        if not answer: 
+            my_answer = "Please enter a valid number."
+            color = 'warning'
+            return render(request, 'subtract.html', {
+            'answer': answer,
+            'my_answer': my_answer,
+            'num_1': num_1,
+            'num_2': num_2,
+            'color': color
+        })
+
         correct_answer = int(old_num_1) - int(old_num_2)
         if int(answer) == correct_answer:
             my_answer = "Correct! " + old_num_1 + " - " + old_num_2 + " = " + str(correct_answer)
@@ -64,7 +88,85 @@ def subtract(request):
     })
 
 def multiply(request):
-    return render(request, 'multiply.html', {})
+    from random import randint
+    num_1 = randint(0, 9)
+    num_2 = randint(0, 9)
+
+    if request.method == "POST":
+        answer = request.POST['answer']
+        old_num_1 = request.POST['old_num_1']
+        old_num_2 = request.POST['old_num_2']
+
+        # error handling if no answer submitted
+        if not answer: 
+            my_answer = "Please enter a valid number."
+            color = 'warning'
+            return render(request, 'multiply.html', {
+            'answer': answer,
+            'my_answer': my_answer,
+            'num_1': num_1,
+            'num_2': num_2,
+            'color': color
+        })
+
+        correct_answer = int(old_num_1) * int(old_num_2)
+        if int(answer) == correct_answer:
+            my_answer = "Correct! " + old_num_1 + " * " + old_num_2 + " = " + str(correct_answer)
+            color = "success"
+        else:
+            my_answer = "Wrong :( " + old_num_1 + " * " + old_num_2 + " does not equal " + answer
+            color = "danger"
+        return render(request, 'multiply.html', {
+            'answer': answer,
+            'my_answer': my_answer,
+            'num_1': num_1,
+            'num_2': num_2,
+            'color': color,
+        })
+
+    return render(request, 'multiply.html', {
+        'num_1': num_1,
+        'num_2': num_2,
+    })
 
 def divide(request):
-    return render(request, 'divide.html', {})
+    from random import randint
+    num_1 = randint(0, 9)
+    num_2 = randint(1, 9)
+
+    if request.method == "POST":
+        answer = request.POST['answer']
+        old_num_1 = request.POST['old_num_1']
+        old_num_2 = request.POST['old_num_2']
+
+        # error handling if no answer submitted
+        if not answer: 
+            my_answer = "Please enter a valid number."
+            color = 'warning'
+            return render(request, 'divide.html', {
+            'answer': answer,
+            'my_answer': my_answer,
+            'num_1': num_1,
+            'num_2': num_2,
+            'color': color
+        })
+
+        correct_answer = round(int(old_num_1) / int(old_num_2), 2)
+        if float(answer) == correct_answer:
+            my_answer = "Correct! " + old_num_1 + " / " + old_num_2 + " = " + str(correct_answer)
+            color = "success"
+        else:
+            my_answer = "Wrong :( " + old_num_1 + " / " + old_num_2 + " does not equal " + answer + "it equals " + str(correct_answer),
+            color = "danger"
+        return render(request, 'divide.html', {
+            'answer': answer,
+            'my_answer': my_answer,
+            'num_1': num_1,
+            'num_2': num_2,
+            'color': color,
+        })
+
+    return render(request, 'divide.html', {
+        'num_1': num_1,
+        'num_2': num_2,
+    })
